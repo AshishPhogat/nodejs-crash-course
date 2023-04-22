2db459fad9333b4af9aeb6e2a792e79808b171cd
Ashish2024nsut <ashishphog.ug20@gmail.com>
2023-04-22 09:54:53 +0530

    first commit

---
+++ stream.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const readstream = fs.createReadStream('./docs/blog3.txt',{encoding : 'utf8'});
+const writestream = fs.createWriteStream('./docs/blog4.txt');
+
+
+// readstream.on('data',(chunk)=>{
+//     console.log("---------NEW CHUNK-------");
+//     console.log(chunk);
+//     writestream.write('\n NEW CHUNK \n');
+//     writestream.write(chunk);
+// });
+
+//piping
+
+readstream.pipe(writestream);
