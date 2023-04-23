29a7df02bf7957aa39df120218204348c061501d
AshishPhogat <ashishphog@gmail.com>
2023-04-23 14:36:56 +0530

    created index view using ejs

--- app.js
+++ app.js
@@ -9,24 +9,28 @@ app.set('view engine','ejs');
 // app.set('views','myviews'); ->to set a diff views folder other than the default 'views'
 
 app.get('/',(req,res)=>{
-    console.log(req.url);
-    res.sendFile('./views/index.html',{root : __dirname});
+    const blogs = [
+        {title:'yoshi finds eggs' , snippet:'hello ji'},
+        {title: 'Mario finds stars',snippet:'stars mille mujhe'},
+        {title: 'How to defeat browser',snippet:'the net ninja crash course karo'}
+    ];
+
+    res.render('index',{title : 'Home',blogs});
 })
 
 app.get('/about',(req,res)=>{
     console.log(req.url);
-    res.sendFile('./views/about.html',{root : __dirname});
+    res.render('about',{title : 'About'});
 })
 
-
-//redirects
-app.get('/about-me',(req,res)=>{
-    res.redirect("/about");
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title : 'Create a new Blog'});
 })
+
 
 //404 page
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root : __dirname})
+    res.status(404).render('404',{title : '404'});
 })
 
 // listen to requests
