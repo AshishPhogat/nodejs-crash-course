6308ebbef8f6c23bd298d38a36f411f85dbcfd84
AshishPhogat <ashishphog@gmail.com>
2023-05-17 09:43:28 +0530

    mongodb connection and some routes

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blogs');
 // express app
 const app = express();
+
+//CONNECT TO MONGODB
+const dbURI = "mongodb+srv://ashishphogug20:788zipvkn3Wttk1A@cluster0.qll3ru3.mongodb.net/blogs?retryWrites=true&w=majority"
+mongoose.connect(dbURI,{useNewUrlParser: true,useUnifiedTopology: true}).then((result)=>{console.log("database connection successful")
+   app.listen(3000,()=>{
+    console.log('server running on the port 3000');
+})
+}).catch((err)=>{console.log(err)});
 
 //register view engine
 app.set('view engine','ejs');
@@ -31,6 +41,41 @@ app.get('/blogs/create',(req,res)=>{
     res.render('create',{title : 'Create a new Blog'});
 })
 
+//mongodb and mongoose sandbox api
+app.get('/add-blog',(req,res)=>{
+    const blog = new Blog({
+        title : 'new blog',
+        snippet: 'about my new blog',
+        body : 'more about my new blog'
+    });
+
+    blog.save().then((result)=>{
+        res.send(result);
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+});
+
+//get all blogs
+app.get('/all-blogs',(req,res)=>{
+    Blog.find().then((result)=>{
+        res.send(result);
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+});
+
+//single blog by id
+app.get('/single-blog/:id',(req,res)=>{
+    Blog.findById(req.params.id).then((result)=>{
+        res.send({result});
+    }).catch((err)=>{
+        console.log(err);
+    });
+})
+
 
 //404 page
 app.use((req,res)=>{
@@ -38,6 +83,3 @@ app.use((req,res)=>{
 })
 
 // listen to requests
-app.listen(3000,()=>{
-    console.log('server running on the port 3000');
-})
